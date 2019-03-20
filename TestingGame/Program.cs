41ea67fe457d66deb1f6a72595c8e2e673beb07e
David Basarab <dbasarab617@gmail.com>
2019-03-19 22:11:30 -0400

    more basic stuff

--- TestingGame/Program.cs
+++ TestingGame/Program.cs
@@ -2,21 +2,40 @@
 using System.Threading.Tasks;
 using Humanizer;
 using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
 
 #pragma warning disable 4014
 
 namespace TestingGame
 {
-	public class MainWindow : GameWindow { }
+	public class MainWindow : GameWindow
+	{
+		public MainWindow()
+			: base(1280, 720, GraphicsMode.Default, "TestingGame", GameWindowFlags.Default, DisplayDevice.Default, 4, 0, GraphicsContextFlags.ForwardCompatible)
+		{
+			Title += $": OpenGL Version: {GL.GetString(StringName.Version)}";
+		}
+
+		protected override void OnResize(EventArgs e) => GL.Viewport(0, 0, Width, Height);
+
+		protected override void OnLoad(EventArgs e)
+		{
+			Console.WriteLine("On Load");
+
+			CursorVisible = true;
+		}
+	}
 
 	public class ProgramSender
 	{
 		private static bool running = true;
 
-		public static async Task Main(string[] args)
+		[STAThread]
+		public static void Main(string[] args)
 		{
 			Console.CancelKeyPress += OnCancel;
-			
+
 			new MainWindow().Run(60);
 
 			//await WaitForExit();
