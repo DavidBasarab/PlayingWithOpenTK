525fe96a058f9a265372374259ccc7157d2f3a99
David Basarab <dbasarab617@gmail.com>
2019-04-12 15:07:29 -0400

    Formatting

--- MongoGame/Game1.cs
+++ MongoGame/Game1.cs
@@ -29,9 +29,7 @@ namespace MongoGame
 
 		protected override void Update(GameTime gameTime)
 		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
-				ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
-																	Keys.Escape)) Exit();
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
 
 			base.Update(gameTime);
 		}
