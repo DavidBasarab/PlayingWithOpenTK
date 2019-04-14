2fe613af7026a32b09cfb15576b76ee6dfb0c0dc
David Basarab <dbasarab617@gmail.com>
2019-04-14 16:56:01 -0400

    Moving objects around

--- TestingGame/MainWindow.cs
+++ TestingGame/MainWindow.cs
@@ -10,56 +10,6 @@ using OpenTK.Input;
 
 namespace TestingGame
 {
-	public class ShapeFactory
-	{
-		public static List<Vertex> CreateSolidCube(float side, Color4 color)
-		{
-			side = side / 2f; // half side - and other half
-
-			var vertices = new List<Vertex>
-							{
-								new Vertex(new Vector4(-side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, side, 1.0f), color),
-								new Vertex(new Vector4(side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, side, 1.0f), color),
-								new Vertex(new Vector4(side, side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(side, side, -side, 1.0f), color),
-								new Vertex(new Vector4(-side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, side, 1.0f), color),
-								new Vertex(new Vector4(-side, side, side, 1.0f), color),
-								new Vertex(new Vector4(side, -side, side, 1.0f), color),
-								new Vertex(new Vector4(side, side, side, 1.0f), color)
-							};
-
-			return vertices;
-		}
-	}
-
 	//http://dreamstatecoding.blogspot.com/2017/02/opengl-4-with-opentk-in-c-part-6.html
 	public class MainWindow : GameWindow
 	{
