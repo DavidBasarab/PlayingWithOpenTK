02d9e536d7a746ef23d2b91a994677ac3619dd3c
David Basarab <dbasarab617@gmail.com>
2019-04-14 13:35:51 -0400

    Created RenderObject

--- TestingGame/MainWindow.cs
+++ TestingGame/MainWindow.cs
@@ -22,6 +22,85 @@ namespace TestingGame
 		}
 	}
 
+	public class RenderObject : IDisposable
+	{
+		private readonly int buffer;
+
+		private readonly int vertexArray;
+		private readonly int verticeCount;
+		private bool initialized;
+
+		public RenderObject(Vertex[] vertices)
+		{
+			verticeCount = vertices.Length;
+
+			vertexArray = GL.GenVertexArray();
+			buffer = GL.GenBuffer();
+
+			GL.BindVertexArray(vertexArray);
+			GL.BindBuffer(BufferTarget.ArrayBuffer, vertexArray);
+
+			// create first buffer: vertex
+			GL.NamedBufferStorage(
+								buffer,
+								Vertex.Size * vertices.Length,   // the size needed by this buffer
+								vertices,                        // data to initialize with
+								BufferStorageFlags.MapWriteBit); // at this point we will only write to the buffer
+
+			GL.VertexArrayAttribBinding(vertexArray, 0, 0);
+			GL.EnableVertexArrayAttrib(vertexArray, 0);
+
+			GL.VertexArrayAttribFormat(
+										vertexArray,
+										0,                      // attribute index, from the shader location = 0
+										4,                      // size of attribute, vec4
+										VertexAttribType.Float, // contains floats
+										false,                  // does not need to be normalized as it is already, floats ignore this flag anyway
+										0);                     // relative offset, first item
+
+			GL.VertexArrayAttribBinding(vertexArray, 1, 0);
+
+			GL.EnableVertexArrayAttrib(vertexArray, 1);
+
+			GL.VertexArrayAttribFormat(
+										vertexArray,
+										1,                      // attribute index, from the shader location = 1
+										4,                      // size of attribute, vec4
+										VertexAttribType.Float, // contains floats
+										false,                  // does not need to be normalized as it is already, floats ignore this flag anyway
+										16);                    // relative offset after a vec4
+
+			// link the vertex array and buffer and provide the stride as size of Vertex
+			GL.VertexArrayVertexBuffer(vertexArray, 0, buffer, IntPtr.Zero, Vertex.Size);
+
+			initialized = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		public void Render()
+		{
+			GL.BindVertexArray(vertexArray);
+			GL.DrawArrays(PrimitiveType.Triangles, 0, verticeCount);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing) return;
+
+			if (!initialized) return;
+
+			GL.DeleteVertexArray(vertexArray);
+			GL.DeleteBuffer(buffer);
+
+			initialized = false;
+		}
+	}
+
 	//http://dreamstatecoding.blogspot.com/2017/02/opengl-4-with-opentk-in-c-part-5.html
 	public class MainWindow : GameWindow
 	{
